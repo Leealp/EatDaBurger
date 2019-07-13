0261045c10bbb3de6e0e70dfb4dcd0d47d13faf6
AlphaLy <lyalpha2401@gmail.com>
2019-07-12 20:13:12 -0500

    finalized with server.js file

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var path = require('path');
+var bodyParser = require("body-parser");
+var methodOverride = require('method-override');
+var PORT = process.env.PORT || 8081;
+
+var app = express();
+
+// serve static content for the app and set up body-parser
+app.use(express.static(__dirname+'/public'));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
+
+// set up Handlebars
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({
+  defaultLayout: "main"
+}));
+
+// app.set('views', path.join(__dirname, 'views'));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them
+var routes = require("./controllers/routes.js");
+app.use('/',routes);
+
+// start the server listening
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
+});
+
