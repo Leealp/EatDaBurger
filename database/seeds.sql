b9825c62a0435d46b775bc7403c45637f420396b
AlphaLy <lyalpha2401@gmail.com>
2019-07-12 19:42:34 -0500

    created a schema and a seed data for my database

---
+++ database/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("cheese burgerr", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("hamburgerr", TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("pizza burger", FALSE);
