294431187c6c67b8080940c939a9d0fb4ab40bea
AlphaLy <lyalpha2401@gmail.com>
2019-07-12 19:51:51 -0500

    created a helper function to create an array of questions mark with placeholdersearch values and turns it into a string.

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,15 @@
 // set up a custom object-relational mapping to MySQL database and export for use elsewhere
 
 var connection = require("../config/connection.js");
+
+
+// Helper function to create an array of question marks ["?", "?", "?"] for placeholder search values and turns it into a string.
+
+function printQuestionMarks(num) {
+  var arr = [];
+  
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+  return arr.toString();
+}
